92d3af338ce2696c2fde38ea7768203b68a0f77f
jmargutti <jacopo.margutti@gmail.com>
2022-08-22 16:28:17 +0200

    update demo

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ st.header("anonymization-app")
 st.markdown("Remove personally identifiable information from text.")
 st.markdown("Built with love by [NLRC 510](https://www.510.global/). See [the project on GitHub](https://github.com/rodekruis/anonymization-app).")
 text = st.text_area('Input', 'Hello my name is Anna and my phone number is +12345678910.')
-submit = st.button('Remove personally identifiable information')
+submit = st.button('Anonymize')
 if submit:
 
     url = 'https://anonymization-app.azurewebsites.net/anonymize/'
