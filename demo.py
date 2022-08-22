d7d2c1a274783df9176e25cae5e84f64f3948c48
jmargutti <jacopo.margutti@gmail.com>
2022-08-22 16:26:42 +0200

    update demo

--- demo.py
+++ demo.py
@@ -1,23 +1,21 @@
 import streamlit as st
 import requests
 
-st.subheader("Output")
-url = 'https://anonymization-app.azurewebsites.net/'
-myobj = {'text': 'Hello my name is Anna and my phone number is +12345678.'}
-x = requests.post(url, json=myobj)
-print(x)
+st.header("anonymization-app")
+st.markdown("Remove personally identifiable information from text.")
+st.markdown("Built with love by [NLRC 510](https://www.510.global/). See [the project on GitHub](https://github.com/rodekruis/anonymization-app).")
+text = st.text_area('Input', 'Hello my name is Anna and my phone number is +12345678910.')
+submit = st.button('Remove personally identifiable information')
+if submit:
 
-# st.header("anonymization-app")
-# st.subheader("Input")
-# text = st.text_area('', 'Hello my name is Anna and my phone number is +12345678.')
-# submit = st.button('Remove personally identifiable information')
-# if submit:
-#
-#     st.subheader("Output")
-#     url = 'https://anonymization-app.azurewebsites.net/'
-#     myobj = {'text': text}
-#
-#     with st.spinner(text="This may take a moment..."):
-#         x = requests.post(url, json=myobj)
-#
-#     st.write(x.text)
+    url = 'https://anonymization-app.azurewebsites.net/anonymize/'
+    payload = {'text': text}
+
+    with st.spinner(text="This may take a moment..."):
+        result = requests.post(url, json=payload).json()
+        if 'anonymized_text' in result.keys():
+            text = result['anonymized_text']
+        else:
+            text = "An error occurred, please try again later or [contact us](mailto:support@510.global)!"
+
+    st.text_area('Output', text)
